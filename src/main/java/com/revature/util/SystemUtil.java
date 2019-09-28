8459106c613ffd9bc49a710e3878670146a72b9d
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-09-28 17:36:33 -0400

    Added SystemUtil and LoginMenuImplTest

---
+++ src/main/java/com/revature/util/SystemUtil.java
@@ -0,0 +1,9 @@
+package com.revature.util;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class SystemUtil {
+	public static Scanner scanner = new Scanner(System.in);
+	public static PrintStream sysout = new PrintStream(System.out);
+}
---
+++ src/test/java/com/revature/test/LoginMenuImplTest.java
@@ -0,0 +1,38 @@
+package com.revature.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.revature.services.LoginMenuImpl;
+
+public class LoginMenuImplTest {
+	private LoginMenuImpl loginImpl;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		loginImpl = new LoginMenuImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
