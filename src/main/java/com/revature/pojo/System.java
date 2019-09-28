a9b24b61bb77fb8bd64ba24217119e349968ba19
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-09-28 15:01:51 -0400

    Added System class

---
+++ src/main/java/com/revature/pojo/System.java
@@ -0,0 +1,10 @@
+package com.revature.pojo;
+
+import java.util.HashMap;
+
+public class System {
+	private User user;
+	private Lot lot;
+	private HashMap<String, Payment> allPaymentsByUsername;
+	private HashMap<String, Offer> allOffersByUsername;
+}
